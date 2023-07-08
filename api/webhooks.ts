abe755a9b685ef6f43037018a43c8683adbdc5e4
Tomas Pocius <pocius.tomastp@gmail.com>
2023-07-09 02:01:06 +0300

    fixing the stripe implementation

--- api/webhooks.ts
+++ api/webhooks.ts
@@ -8,7 +8,28 @@ export const config = {
   },
 };
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+const allowCors =
+  (fn: any) => async (req: VercelRequest, res: VercelResponse) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+
+async function handler(req: VercelRequest, res: VercelResponse) {
   // check the request method and ensure you only accept POST requests.
   if (req.method === "POST") {
     let event: { [key: string]: any };
@@ -50,3 +71,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     //   console.log(event.type)
   }
 }
+
+export default allowCors(handler);
